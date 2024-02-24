ee1c6ce1762fae7001c173bb6e06763f383ad810
lokeshnaik-01 <lavadiyalokeshnaik@gmail.com>
2024-02-24 17:17:41 +0530

    first commit

---
+++ src/main/java/com/lokeshSpring/lokeshSpringCourse/LokeshSpringCourseApplication.java
@@ -0,0 +1,13 @@
+package com.lokeshSpring.lokeshSpringCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokeshSpringCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokeshSpringCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lokeshSpring/lokeshSpringCourse/LokeshSpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokeshSpring.lokeshSpringCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokeshSpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
