432a1562a8c9de48d6129e7f7c1d594464c837ba
lokeshnaik-01 <lavadiyalokeshnaik@gmail.com>
2024-02-24 17:35:52 +0530

    added comments about restcontroller

--- src/main/java/com/lokeshSpring/lokeshSpringCourse/rest/FunRestController.java
+++ src/main/java/com/lokeshSpring/lokeshSpringCourse/rest/FunRestController.java
@@ -3,6 +3,8 @@ package com.lokeshSpring.lokeshSpringCourse.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// here we created a package called as rest controller and init we are initializing the controller
+// when ever a get request is called sayHello method will be executed
 @RestController
 public class FunRestController {
     // exposr "/" that returns "Hello World"
